fbbd7707ba8f495a3197f44df962bba9a515bf70
Eva Pace <eba.pachi@gmail.com>
2024-09-02 20:07:45 -0300

    remove redundant comments in project_setup.sh

--- project_setup.sh
+++ project_setup.sh
@@ -71,38 +71,29 @@ echo '.{
 mkdir src
 
 # coppied from ../examples/core/basic_window.zig 
-echo '// raylib-zig (c) Nikolas Wipper 2023
-
-const rl = @import("raylib");
+echo 'const rl = @import("raylib");
 
 pub fn main() anyerror!void {
-    // Initialization
-    //--------------------------------------------------------------------------------------
     const screenWidth = 800;
     const screenHeight = 450;
 
     rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
     defer rl.closeWindow(); // Close window and OpenGL context
 
-    rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    rl.setTargetFPS(60);
 
-    // Main game loop
-    while (!rl.windowShouldClose()) { // Detect window close button or ESC key
+    // Detect window close button or ESC key
+    while (!rl.windowShouldClose()) {
         // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        // ...
 
         // Draw
-        //----------------------------------------------------------------------------------
         rl.beginDrawing();
         defer rl.endDrawing();
 
         rl.clearBackground(rl.Color.white);
 
         rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.light_gray);
-        //----------------------------------------------------------------------------------
     }
 }' >> src/main.zig
 
